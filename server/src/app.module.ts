59e24c3084ba86c2a960ec0a8182e876ce81473d
Redchenko Evgeniy <nutshell2088@yandex.ru>
2026-02-01 23:40:17 +0300

    Add ConfigModule and .env files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,21 @@
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}.local`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'jwt_auth_dev',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
